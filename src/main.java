f29b5979550d1c048f9cdc33bcfe585dadb015d5
TasfiqulGhani <tasfiqulghani@gmail.com>
2017-01-04 22:51:48 +0600

    codes

---
+++ src/main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		
+		
+		
+	   System.out.println("Please enter a number: ");
+		int numberOne=sc.nextInt();
+		System.out.println("Please enter a number: ");
+		int numberTwo=sc.nextInt();
+		System.out.println("The result is: ");
+		int result=numberOne+numberTwo;
+		System.out.print(result);
+		
+		
+	}
+
+}
